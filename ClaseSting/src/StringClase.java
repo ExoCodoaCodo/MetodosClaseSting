b732b60eec556e7c8e6793772908f66c615aaccf
alejandro <alejandro>
2017-08-29 14:23:03 -0300

    primera vez que se sube

---
+++ ClaseSting/src/StringClase.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Español
+ */
+public class StringClase {
+    
+    
+    
+    public static void main(String[]args){
+    
+    String palabra="Curso de Programacion en Java Sr";
+    
+    
+        System.out.println(palabra.charAt(12));
+        
+        System.out.println(palabra.charAt(0));
+        
+        System.out.println(palabra.endsWith("n"));
+        
+        System.out.println(palabra.startsWith("n"));
+        
+        System.out.println(palabra.equals("n"));
+        
+        System.out.println(palabra.length());
+        
+        System.out.println(palabra.indexOf("larg"));
+        
+        System.out.println(palabra.replace('a','e'));
+        
+        System.out.println(palabra.toLowerCase());
+        
+        System.out.println(palabra.toUpperCase());
+        
+    
+    
+    
+    
+    }
+    
+}
